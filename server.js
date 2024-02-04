9cab29dcc87a609ae83dfebd6d1f773705e94dc3
Jennifer Winter <jennwintr@gmail.com>
2024-02-04 17:56:18 -0500

    date format

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +10,14 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+app.use(require("./routes"));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
