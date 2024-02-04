7af2070e6d3093db8f5dfacbe6fde9c8840300e5
Jennifer Winter <jennwintr@gmail.com>
2024-02-03 21:07:13 -0500

    update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -46,28 +46,19 @@ const userController = {
         })
   },
   // update a user
-  async updateUser(req, res) {
-    try {
-      const dbUserData = await User.findOneAndUpdate(
-        { _id: req.params.userId },
-        {
-          $set: req.body,
-        },
-        {
-          runValidators: true,
-          new: true,
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
         }
-      );
-
-      if (!dbUserData) {
-        return res.status(404).json({ message: 'No user with this id!' });
-      }
-
-      res.json(dbUserData);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
   },
   // delete user (BONUS: and delete associated thoughts)
   async deleteUser(req, res) {
